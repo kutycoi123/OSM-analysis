17033eb6545958d7caa2c9f1dfa37f4ce4d64659
kutycoi123 <tln3@sfu.ca>
2020-07-25 23:33:11 -0700

    Added scripts to get amenities cluster and to filter out entertainment amenities

---
+++ entertainments_nospark.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+entertainments = ['arts_centre', 'bistro', 'nightclub', 'bbq', 'car_rental',
+                      'leisure', 'park', 'restaurant', 'bar', 'casino', 'gambling',
+                      'cafe', 'theatre', 'stripclub', 'pub']
+
+def main(inputs, output):
+	df = pd.read_json(inputs, lines=True)
+	df = df[df['amenity'].isin(entertainments)]
+	#print(df['amenity'])
+	df.to_json(output, orient='records', compression='gzip', lines=True)
+	pass
+
+if __name__ == '__main__':
+	inputs = sys.argv[1]
+	output = sys.argv[2]
+	main(inputs, output)
---
+++ nearby_amenities.py
@@ -0,0 +1,58 @@
+import sys
+import numpy as np
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.sql import SparkSession, functions, types
+from pyspark.sql.functions import lower
+from pyspark.ml import Pipeline
+import matplotlib.pyplot as plt
+from pyspark.ml.clustering import KMeans, KMeansModel
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.mllib.tree import RandomForest
+from pyspark.mllib.linalg import Vectors
+# add more functions as necessary
+schema = types.StructType([
+	types.StructField('lat', types.DoubleType(), nullable=False),
+	types.StructField('lon', types.DoubleType(), nullable=False),
+	types.StructField('timestamp', types.TimestampType(), nullable=False),
+	types.StructField('amenity', types.StringType(), nullable=False),
+	types.StructField('name', types.StringType(), nullable=True),
+	types.StructField('tags', types.MapType(types.StringType(), types.StringType()), nullable=False),
+])
+RANDOM_SEED = 13579
+TRAINING_DATA_RATIO = 0.7
+RF_NUM_TREES = 3
+RF_MAX_DEPTH = 4
+RF_MAX_BINS = 32
+def main(inputs, output):
+    # main logic starts here
+	data = spark.read.json(inputs, schema=schema)
+	assembler = VectorAssembler(
+		inputCols = ['lat', 'lon'],
+		outputCol='features')
+	kmeans_estimator = KMeans().setK(10)\
+		.setFeaturesCol("features").setPredictionCol('prediction')
+	pipeline = Pipeline(stages=[assembler, kmeans_estimator])
+	model = pipeline.fit(data)
+	predictions = model.transform(data)
+	plt.scatter(predictions.select('lat').collect(),
+				predictions.select('lon').collect(),
+				c=predictions.select('prediction').collect(),
+				cmap='Set1', edgecolor='k', s=20)
+	centers = model.stages[-1].clusterCenters()
+	for center in centers:
+		print(center)
+
+    #plt.show();return
+	plt.savefig(output)	
+
+
+if __name__ == '__main__':
+	inputs = sys.argv[1]
+	output = sys.argv[2]
+	spark = SparkSession.builder.appName('example code').getOrCreate()
+	assert spark.version >= '2.4' # make sure we have Spark 2.4+
+	spark.sparkContext.setLogLevel('WARN')
+    #sc = spark.sparkContext
+
+	main(inputs, output)
