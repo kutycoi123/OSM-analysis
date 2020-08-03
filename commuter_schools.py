7002c11db138ab05caa1652e57343df49677ba12
Bolshevik35 <hongquan03051997@gmail.com>
2020-08-03 15:00:09 -0700

    clustering transportation

--- commuter_schools.py
+++ commuter_schools.py
@@ -5,9 +5,13 @@
 
 import sys
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, AgglomerativeClustering, AffinityPropagation
+
 from pyspark.sql import SparkSession, functions, types, Row
+import matplotlib.pyplot as plt
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import LinearSVC
+from pyspark.ml.clustering import KMeans, KMeansModel
+from pyspark.ml import Pipeline
 spark = SparkSession.builder.appName('OSM point of interest extracter').getOrCreate()
 assert spark.version >= '2.4' # make sure we have Spark 2.4+
 spark.sparkContext.setLogLevel('WARN')
@@ -23,23 +27,31 @@ amenity_schema = types.StructType([
     types.StructField('tags', types.MapType(types.StringType(), types.StringType()), nullable=False),
 ])
 
-transportations_schools = ['bicycle_parking', 'bus_station', 'parking', 'fuel',
-                    'car_sharing', 'ferry_terminal', 'parking_entrance', 'seaplane terminal']
-# transportations = ['bus_station']
 
-# transportations =['bicycle_parking']
 def main(inputs, output):
     poi = spark.read.json(inputs, schema=amenity_schema)
     poi = poi.filter((poi['lon'] > -123.5) & (poi['lon'] < -122))
     poi = poi.filter((poi['lat'] > 49) & (poi['lat'] < 49.5))
-    #poi = poi.coalesce(1) # ~1MB after the filtering 
+    # poi.show(100)
+    # schools = poi.filter(poi['amenity'] == 'school')
+    # prediction_data = poi.filter((functions.size('tags') > 0) & (poi['name'] != 'null'))
 
-    transportations_data = poi.filter(poi.amenity.isin(transportations_schools))
-    # transportations_data = transportations_data.filter(functions.size('tags') > 0)
-    plt.scatter(transportations_data.select('lon').collect(), transportations_data.select('lat').collect(), s = 20, marker='.')
+    # training, validation = schools.randomSplit([.7, .3])
+
+    stage1 = VectorAssembler(inputCols =['lon', 'lat'], outputCol='features')
+    stage2 = KMeans().setK(10).setFeaturesCol("features").setPredictionCol('prediction')
+    pipeline = Pipeline(stages=[stage1, stage2])
+    model = pipeline.fit(poi)
+    predictions = model.transform(poi)
+    plt.scatter(predictions.select('lon').collect(),
+                predictions.select('lat').collect(),
+                c=predictions.select('prediction').collect(),
+                cmap='Set1', edgecolor='k', s=20)
+    plt.savefig('potential_bus_zone')
     # plt.show()
-    plt.savefig('transportations_schools_map')
-    transportations_data.write.json(output, mode='overwrite', compression='gzip')
+
+    #poi = poi.coalesce(1) # ~1MB after the filtering 
+    # poi.write.json(output, mode='overwrite', compression='gzip')
 
 
 if __name__ == '__main__':
