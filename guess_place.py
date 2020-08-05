57b202b887d2ba7c687c81391cfe68ddf5467957
kutycoi123 <tln3@sfu.ca>
2020-08-04 23:26:18 -0700

    Guess multiple places

--- guess_place.py
+++ guess_place.py
@@ -8,15 +8,28 @@ def dist(lat1, lon1, lat2, lon2):
     a = 0.5 - np.cos((lat2-lat1)*p)/2 + np.cos(lat1*p) * np.cos(lat2*p) * (1-np.cos((lon2-lon1)*p))/2
     return 12742 * np.arcsin(np.sqrt(a))
 
-def main(inputs, output=None):
-	lat,lon = 49.268737,-123.045729
+def main(inputs, locations, output=None):
+	#lat,lon = 49.268737,-123.045729
 	df = pd.read_json(inputs, orient='records', compression='gzip', lines=True)
+	loc = pd.read_csv(locations)
+	lat = loc['latitude'].to_numpy()
+	lon = loc['longitude'].to_numpy()
 	#print(df.head(50))
-	df['dist'] = df.apply(lambda x: dist(x.lat, x.lon, lat, lon), axis=1)
-	places = df[df['dist'] < 0.2]
-	print(places.columns)
-	print(places.head(2))
+	places = None
+	for i in range(len(lat)):
+		lat_value = lat[i]
+		lon_value = lon[i]
+		df['dist'] = df.apply(lambda x: dist(x.lat, x.lon, lat_value, lon_value), axis=1)
+		if places is not None:
+			places = pd.concat([places, df[df['dist'] < 0.2]])
+		else:
+			places = df[df['dist'] < 0.05]
+	places = places.drop_duplicates(subset=['lat','lon'], keep='first')
+	print(places.head(10))
+	places.to_json(output, orient='records', lines=True)
 
 if __name__ == '__main__':
 	inputs = sys.argv[1]
-	main(inputs)
+	locations = sys.argv[2]
+	output = sys.argv[3]
+	main(inputs, locations, output)
