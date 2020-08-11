def396fee08b25de2e9e287aa049a5864b7640c7
kutycoi123 <tln3@sfu.ca>
2020-08-11 13:35:52 -0700

    Fixed print statement

--- data-extraction/get_ratings.py
+++ data-extraction/get_ratings.py
@@ -8,12 +8,13 @@ api_key = 'AIzaSyAoFo2n3NfRB7n3a8ltVufvwPAj65J4zZU'
 def get_ratings(x):
 	lat, lon, name = x['lat'], x['lon'], x['name']
 	if name:
+		original_name = name
 		name = urllib.parse.quote(name)
 		url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius={}&name={}&key={}"\
 				.format(lat, lon, radius, name, api_key)
 		data = json.load(urllib.request.urlopen(url))
 		if len(data['results']) > 0:
-			print("Getting results")
+			print("{}: {}".format(original_name, data['results'][0]['rating']))
 			return data['results'][0]['rating']
 	return None
 
