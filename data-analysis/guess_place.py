ae3fe14b2dcf3341e2eb4451fde5d655120aaf0b
kutycoi123 <tln3@sfu.ca>
2020-08-11 12:57:43 -0700

    Added readme

--- data-analysis/guess_place.py
+++ data-analysis/guess_place.py
@@ -2,13 +2,16 @@ import sys
 import pandas as pd
 import numpy as np
 from math import pi
+inputs = "../entertainments-vancouver.json.gzip"
+locations = "locations.csv"
+output = "guessed-places.json"
 def dist(lat1, lon1, lat2, lon2):
     # Reference: https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula/21623206
     p = pi/180
     a = 0.5 - np.cos((lat2-lat1)*p)/2 + np.cos(lat1*p) * np.cos(lat2*p) * (1-np.cos((lon2-lon1)*p))/2
     return 12742 * np.arcsin(np.sqrt(a))
 
-def main(inputs, locations, output=None):
+def main():
 	df = pd.read_json(inputs, orient='records', compression='gzip', lines=True)
 	loc = pd.read_csv(locations)
 	lat = loc['latitude'].to_numpy()
@@ -27,7 +30,4 @@ def main(inputs, locations, output=None):
 	places.to_json(output, orient='records', lines=True)
 
 if __name__ == '__main__':
-	inputs = sys.argv[1]
-	locations = sys.argv[2]
-	output = sys.argv[3]
-	main(inputs, locations, output)
+	main()
